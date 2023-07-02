1289efc949b17a2fba0752e37952de6bf7a8ba9d
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-07-02 10:02:23 -0400

    associations for bands and meet and greet added

--- models/band.js
+++ models/band.js
@@ -1,39 +1,44 @@
-// DEPENDENCIES
-const { Sequelize, DataTypes,  Model } = require('sequelize')
-
-
-// MODEL
-class Band extends Model{}
-    
-Band.init({
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Band extends Model {
+        static associate({ MeetGreet }) {
+          // meet and greets 
+          Band.hasMany(MeetGreet, {
+            foreignKey: "band_id",
+            as: "meet_greets"
+          })
+      }
+    }      
+  Band.init({
     band_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     genre: {
-        type: DataTypes.TEXT,
-        allowNull: false
+      type: DataTypes.TEXT,
+      allowNull: false
     },
     available_start_time: {
-        type: DataTypes.DATE,
-        allowNull: false
+      type: DataTypes.DATE,
+      allowNull: false
     },
     end_time: {
-        type: DataTypes.DATE,
-        allowNull: false
+      type: DataTypes.DATE,
+      allowNull: false
     }
   }, {
     sequelize,
     modelName: 'Band',
     tableName: 'bands',
     timestamps: false
-  })
-
-
-// EXPORT
-module.exports = Band
+  });
+  return Band;
+};
